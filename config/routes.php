c0d6ece6242f898c6b1019d29c73b3f7ea29051d
xiangdongdong <xiangdongdong@chelun.com>
2019-09-20 09:50:52 +0800

    1.fix

--- config/routes.php
+++ config/routes.php
@@ -14,13 +14,16 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+//管理员
 Router::addRoute(['GET'], '/admin/{id}', 'App\Controller\AdminController@getsAdmin');
 Router::addRoute(['PUT'], '/admin/', 'App\Controller\AdminController@addAdmin');
-Router::addRoute(['POST','SAVE'], '/admin/', 'App\Controller\AdminController@updateAdmin');
 Router::addRoute(['DELETE'], '/admin/{id}', 'App\Controller\AdminController@deleteAdmin');
+Router::addRoute(['POST','SAVE'], '/admin/', 'App\Controller\AdminController@updateAdmin');
+//登陆
 Router::addRoute(['POST', 'GET'], '/user/login', 'App\Controller\AdminController@loginAdmin');
 Router::addRoute(['GET'], '/user/info', 'App\Controller\AdminController@userInfo');
 Router::addRoute(['POST'], '/user/logout', 'App\Controller\AdminController@logout');
+//普通上传
 Router::addRoute(['POST'], '/common/upload', 'App\Controller\CommonController@upload');
 
 
